3ba3109716af81a658e1f029758e713942f21fda
Orozbeko/MRS0607 <Orozbeko>
2024-06-07 21:40:55 +0600

    first commit

--- about_me/models.py
+++ about_me/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+
+    GENRES_OF_BOOKS = (
+        ("FANTASY", "FANTASY"),
+        ("FICTION", "FICTION"),
+        ("THRILLER", "THRILLER"),
+        ("ROMANCE", "ROMANCE"),
+        ("HORROR", "HORROR")
+    )
+
+    title = models.CharField(max_length=100, verbose_name='Введите название книги')
+    image = models.ImageField(upload_to='books/' , verbose_name='Загрузите фото')
+    author = models.CharField(max_length=100, verbose_name='Введите автора книги')
+    genres_of_books = models.CharField(max_length=100, verbose_name='Выберите жанр книги', choices=GENRES_OF_BOOKS)
+    content = models.TextField(verbose_name='Напишите содержание')
+    review = models.TextField(verbose_name='Напишите отзывы об этой книге')
+    part = models.IntegerField(verbose_name='Напишите какая у вас часть книги')
+    page = models.IntegerField(verbose_name='Напишите сколько страниц у книги')
+    publication_date = models.DateField(verbose_name='Укажите дату публикации книги')
+
+
+    def __str__(self):
+        return f'{self.title}-{self.genres_of_books}'
+    
+
+    class Meta:
+        verbose_name = 'Книгу'
+        verbose_name_plural = 'Книги'
+
+
--- about_me/urls.py
+++ about_me/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from about_me.views import about_me_view, hobby_view, time_view, random_numbers_view
+from . import views
 
 
 urlpatterns = [
-    path('about/', about_me_view),
-    path('myhobby/', hobby_view),
-    path('datatime/', time_view),
-    path('random_numbers/', random_numbers_view),
+    path('books/', views.book_list_view),
+    path('book/<int:id>/', views.book_detail_view),
+    path('about/', views.about_me_views),
+    path('myhobby/', views.hobby_view),
+    path('datatime/', views.time_view),
+    path('random_numbers/', views.random_numbers_view),
 ]
