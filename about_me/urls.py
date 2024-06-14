c141497524377b9bcbbc92066abede2716ed06dd
Orozbeko/MRS0607 <Orozbeko>
2024-06-14 14:13:07 +0600

    Add file

--- about_me/urls.py
+++ about_me/urls.py
@@ -4,5 +4,11 @@ from . import views
 urlpatterns = [
     path('books/', views.book_list_view),
     path('book/<int:id>/', views.book_detail_view),
-    path('', views.book_list_view, name='book_list'),
+    path('book_list/', views.book_list_view, name='book_list'),
+    path('tags/<str:tag_name>/', views.book_list_by_tag, name='book_list_by_tag'),
+    path('create book/', views.create_book_view),
+    path('book/<int:id>/delete/', views.drop_book_view),
+    path('book/<int:id>/update/', views.edit_book_view),
+
+
 ]
--- about_me/views.py
+++ about_me/views.py
@@ -1,6 +1,47 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from about_me.models import Book,Poster,Books
+from about_me.models import Book,Poster,Books,Tag
+from . import forms
+
+
+def edit_book_view(request, id):
+    book_id = get_object_or_404(Books, id=id)
+    if request.method == 'POST':
+        form = forms.BookForm(request.POST, instance=book_id)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('<h3>Book successfully updated!</h3>'
+                                '<a href="books/">На список книг</a>')
+
+    else:
+        form = forms.BookForm(instance=book_id)
+    return render(request, template_name='blog/edit_book.html',
+                  context={
+                      'form': form,
+                      'book_id': book_id
+                  })
+
+
+def drop_book_view(request, id):
+    book_id = get_object_or_404(Book, id=id)
+    book_id.delete()
+    return HttpResponse('Book deleted <a href="books/">На список книг</a>')
+
+
+def create_book_view(request):
+    if request.method == "POST":
+        form = forms.BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('<h3> Book successfully created!</h3>'
+                                '<a href="books/">На список книг</a>')
+    else:
+        form =forms.BookForm()
+    return render(request, template_name='blog/create_book.html',
+                  context={'form': forms})
+
+
+
 
 
 def book_list_view(request):
@@ -38,5 +79,9 @@ def book_list(request):
         books = Book.objects.all()
     return render(request, 'books/book_list.html', {'books': books, 'age_category': age_category})
 
+def book_list_by_tag(request, tag_name):
+    tag = Tag.objects.get(name=tag_name)
+    books = tag.books.all()
+    return render(request, 'book_list.html', {'tag': tag, 'books': books})
 
 
